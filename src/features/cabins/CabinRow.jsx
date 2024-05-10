6db3dd50e2e8bdd9aad0b5240e21f72e73ce7758
zeenat <zeenu.23@gmail.com>
2024-05-10 12:43:56 +0530

    cabin starter files

---
+++ src/features/cabins/CabinRow.jsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+
+const Img = styled.img`
+  display: block;
+  width: 6.4rem;
+  aspect-ratio: 3 / 2;
+  object-fit: cover;
+  object-position: center;
+  transform: scale(1.5) translateX(-7px);
+`;
+
+const Cabin = styled.div`
+  font-size: 1.6rem;
+  font-weight: 600;
+  color: var(--color-grey-600);
+  font-family: "Sono";
+`;
+
+const Price = styled.div`
+  font-family: "Sono";
+  font-weight: 600;
+`;
+
+const Discount = styled.div`
+  font-family: "Sono";
+  font-weight: 500;
+  color: var(--color-green-700);
+`;
