569bfa61d66cc26ba89a35c4d21e0f90809cb16d
zeenat <zeenu.23@gmail.com>
2024-05-16 21:57:45 +0530

    add GlobalStyles import, new styled components (Input, StyledApp), and updated existing components (Button) with additional styles and functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import GlobalStyles from './styles/GlobalStyles'
 
 const H1 = styled.h1`
   font-size: 30px;
@@ -13,14 +14,32 @@ const Button = styled.button`
   padding: 1.2rem 1.6rem;
   font-size: 1.2rem;
   font-weight: 500;
+  cursor: pointer;
+  margin: 20px;
+`
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`
+
+const StyledApp = styled.div`
+  background: orangered;
+  padding: 2rem;
 `
 
 function App() {
   return (
-    <div>
+    <>   
+    <GlobalStyles/>
+    <StyledApp>
       <H1>The Wild Oasis</H1>
-      <Button>Check in</Button>
-    </div>
+      <Button onClick={()=>alert('check in')}>Check in</Button>
+      <Button onClick={() => alert('check out')}>Check out</Button>
+      <Input type='text' placeholder='Number of guests'/>
+      </StyledApp>
+    </>
   )
 }
 
