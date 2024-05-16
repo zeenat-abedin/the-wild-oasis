f084cf82a859170908052f3f641b509040650fdb
zeenat <zeenu.23@gmail.com>
2024-05-16 19:00:15 +0530

    introduced a new styled Button component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,23 @@ import styled from 'styled-components'
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
+  background-color: yellow
+`
+const Button = styled.button`
+  background-color: purple;
+  color: white;
+  border: none;
+  border-radius: 7px;
+  padding: 1.2rem 1.6rem;
+  font-size: 1.2rem;
+  font-weight: 500;
 `
 
 function App() {
   return (
     <div>
       <H1>The Wild Oasis</H1>
+      <Button>Check in</Button>
     </div>
   )
 }
