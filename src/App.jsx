0b5cf10b259d76f99bd3b2459fd04733a30cdfe0
zeenat <zeenu.23@gmail.com>
2024-05-17 23:01:25 +0530

    updated App component to use main element instead of div,
    added type prop to Heading component, and modified styles for Heading and App components.

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Input from './ui/Input'
 import Heading from './ui/Heading'
 
 
-const StyledApp = styled.div`
+const StyledApp = styled.main`
   background: orangered;
   padding: 2rem;
 `
@@ -15,9 +15,13 @@ function App() {
     <>   
     <GlobalStyles/>
     <StyledApp>
-      <Heading>The Wild Oasis</Heading>
+      <Heading type="h1">The Wild Oasis</Heading>
+        
+      <Heading type="h2">Check in and out</Heading>
       <Button onClick={()=>alert('check in')}>Check in</Button>
-      <Button onClick={() => alert('check out')}>Check out</Button>
+        <Button onClick={() => alert('check out')}>Check out</Button>
+        
+      <Heading type="h3">Form</Heading>
       <Input type='text' placeholder='Number of guests'/>
     </StyledApp>
     </>
--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -1,9 +1,14 @@
-import styled from "styled-components"
+import styled, { css } from "styled-components"
+
+const test = css`text-align: center;
+  ${10 > 5 && "background-color:yellow" }
+`
 
 const Heading = styled.h1`
-  font-size: 30px;
+  font-size: 20px;
   font-weight: 600;
   background-color: yellow
+  ${test}
 `
 
 export default Heading;
