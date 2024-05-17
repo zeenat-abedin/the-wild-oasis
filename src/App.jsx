49a61749ff402913df3bcb55f60e351ae77921f5
zeenat <zeenu.23@gmail.com>
2024-05-17 12:01:25 +0530

    add background-color property to Input component.

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ const Button = styled.button`
 
 const Input = styled.input`
   border: 1px solid var(--color-grey-300);
+  background-color: var(--color-grey-0);
   border-radius: 5px;
   padding: 0.8rem 1.2rem;
 `
