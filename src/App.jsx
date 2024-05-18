1cff117046283b3d7da0832ee18500225463c759
zeenat <zeenu.23@gmail.com>
2024-05-18 10:33:22 +0530

    add Row component and wrapped existing elements with it

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import GlobalStyles from './styles/GlobalStyles'
 import Button from './ui/Button'
 import Input from './ui/Input'
 import Heading from './ui/Heading'
-
+import Row from './ui/Row'
 
 const StyledApp = styled.main`
   background: orangered;
@@ -14,15 +14,19 @@ function App() {
   return (
     <>   
     <GlobalStyles/>
-    <StyledApp>
+      <StyledApp>
+      <Row>
       <Heading type="h1">The Wild Oasis</Heading>
-        
+
+      <div>  
       <Heading type="h2">Check in and out</Heading>
       <Button onClick={()=>alert('check in')}>Check in</Button>
-        <Button onClick={() => alert('check out')}>Check out</Button>
-        
+      <Button onClick={() => alert('check out')}>Check out</Button>
+      </div>  
+          
       <Heading type="h3">Form</Heading>
-      <Input type='text' placeholder='Number of guests'/>
+      <Input type='text' placeholder='Number of guests' />
+      </Row>
     </StyledApp>
     </>
   )
