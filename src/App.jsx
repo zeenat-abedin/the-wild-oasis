1fc3212ed609669634b6ca13964921ed34ec6e68
zeenat <zeenu.23@gmail.com>
2024-05-22 00:03:35 +0530

    add PageNotFound and Settings routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Account from './pages/Account'
 import Bookings from './pages/Bookings'
 import Cabins from './pages/Cabins'
 import Login from './pages/Login'
+import PageNotFound from './pages/PageNotFound'
+import Settings from './pages/Settings'
 
 function App() {
   return (
@@ -13,7 +15,10 @@ function App() {
         <Route path="account" element={<Account/>} />
         <Route path="bookings" element={<Bookings/>} />
         <Route path="cabins" element={<Cabins/>} />
-        <Route path="login" element={<Login/>} />
+        <Route path="login" element={<Login />} />
+        <Route path="settings" element={<Settings />} />
+        <Route path="*" element={<PageNotFound />} />
+        
       </Routes>
     </BrowserRouter>
   )
