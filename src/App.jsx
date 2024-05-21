6f883ce984d395f9ea0511d5e4d134db8e7831ed
zeenat <zeenu.23@gmail.com>
2024-05-21 23:41:34 +0530

    add react-router-dom dependency and updated App.jsx to use BrowserRouter, Routes, and Route components; removed styled components and ui components

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,13 @@
-import styled from 'styled-components'
-import GlobalStyles from './styles/GlobalStyles'
-import Button from './ui/Button'
-import Input from './ui/Input'
-import Heading from './ui/Heading'
-import Row from './ui/Row'
-
-const StyledApp = styled.main`
-  padding: 2rem;
-`
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
 
 function App() {
   return (
-  <>   
-  <GlobalStyles/>
-    <StyledApp>
-     <Row type="vertical">
-     <Row type="horizontal">
-      <Heading as="h1">The Wild Oasis</Heading>
-      <div>  
-      <Heading as="h2">Check in and out</Heading>
-      <Button onClick={()=>alert('check in')}>Check in</Button>
-      <Button variation="secondary" size="medium" onClick={() => alert('check out')}>Check out</Button>
-      </div>  
-     </Row>
-        
-    <Row type="vertical">
-    <Heading as="h3">Form</Heading>
-      <form>
-         <Input type='text' placeholder='Number of guests' />
-         <Input type='text' placeholder='Number of guests' />
-      </form>
-    </Row>
-    </Row>    
-    </StyledApp>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="dashboard" element={<Dashboard /> } />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
