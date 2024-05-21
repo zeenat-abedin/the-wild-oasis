71d4927629ae56b09fb723d02bce62f6a1e9ab70
zeenat <zeenu.23@gmail.com>
2024-05-21 23:59:28 +0530

    added new routes for Account, Bookings, Cabins, and Login pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
+import Account from './pages/Account'
+import Bookings from './pages/Bookings'
+import Cabins from './pages/Cabins'
+import Login from './pages/Login'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="dashboard" element={<Dashboard /> } />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="account" element={<Account/>} />
+        <Route path="bookings" element={<Bookings/>} />
+        <Route path="cabins" element={<Cabins/>} />
+        <Route path="login" element={<Login/>} />
       </Routes>
     </BrowserRouter>
   )
