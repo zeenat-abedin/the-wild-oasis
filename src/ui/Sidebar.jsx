31ba9e15c2c36d2e4f0ee1e9edb721caa99a5ca1
zeenat <zeenu.23@gmail.com>
2024-05-22 19:17:51 +0530

    add styled-components import and wrap Sidebar component with StyledSidebar component, applying styles to the aside element

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,9 +1,16 @@
+import styled from "styled-components"
+
+const StyledSidebar = styled.aside`
+  background-color: aliceblue;
+  padding: 3.2rem 2.4rem;
+  border-bottom: 1px solid var(--color-grey-100)
+`
 
 function Sidebar() {
   return (
-    <aside>
+    <StyledSidebar>
       SIDEBAR
-    </aside>
+    </StyledSidebar>
   )
 }
 
