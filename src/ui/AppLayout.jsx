c81629653afe54bad4c1818dcd93056c3c61f01e
zeenat <zeenu.23@gmail.com>
2024-05-22 18:53:15 +0530

    add styled components to AppLayout and Header, replacing default HTML elements with styled equivalents

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,15 +1,21 @@
 import { Outlet } from "react-router-dom"
 import Header from "./Header"
 import Sidebar from "./Sidebar"
+import styled from "styled-components"
+
+const Main = styled.main`
+   background-color: green;
+   padding: 4rem 4.8rem 6.4rem;
+`
 
 function AppLayout() {
   return (
     <div>
       <Header />
       <Sidebar />
-      <main>
+      <Main>
         <Outlet />
-      </main>
+      </Main>
     </div>
   )
 }
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,9 +1,16 @@
+import styled from "styled-components"
+
+const StyledHeader = styled.header`
+  background-color: orangered;
+  padding: 1.2rem 4.8rem;
+  border-bottom: 1px solid var(--color-grey-100)
+`
 
 function Header() {
   return (
-    <header>
+    <StyledHeader>
       HEADER
-    </header>
+    </StyledHeader>
   )
 }
 
