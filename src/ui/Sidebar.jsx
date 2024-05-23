9330605fc7c93c121a19ad5486defec884086f79
zeenat <zeenu.23@gmail.com>
2024-05-23 13:11:03 +0530

    update Sidebar styles and layout

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -3,10 +3,14 @@ import Logo from "./Logo"
 import MainNav from "./MainNav"
 
 const StyledSidebar = styled.aside`
-  /* background-color: blue; */
+  background-color: var(--color-grey-0);
   padding: 3.2rem 2.4rem;
-  border-bottom: 1px solid var(--color-grey-200);
-  grid-row: 1/-1
+  border-right: 1px solid var(--color-grey-100);
+
+  grid-row: 1/-1;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
 `
 
 function Sidebar() {
