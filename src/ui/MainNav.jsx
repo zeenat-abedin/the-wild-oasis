21fd4956dfea05d9b81c5d4dc435b416ea766129
zeenat <zeenu.23@gmail.com>
2024-05-23 12:44:55 +0530

    add new navigation links and icons to MainNav component

--- src/ui/MainNav.jsx
+++ src/ui/MainNav.jsx
@@ -1,6 +1,6 @@
 import { NavLink } from "react-router-dom";
 import { HiOutlineHome } from "react-icons/hi";
-import { HiOutlineCalendarDays } from "react-icons/hi2";
+import { HiOutlineCalendarDays, HiOutlineCog6Tooth, HiOutlineHomeModern,HiOutlineUsers } from "react-icons/hi2";
 import styled from "styled-components";
 
 const NavList = styled.ul`
@@ -58,12 +58,29 @@ function MainNav() {
             <HiOutlineHome />
             <span>Home</span>
           </StyledNavLink>
-        </li>
-        
+        </li>        
         <li>
           <StyledNavLink to="/bookings">
             <HiOutlineCalendarDays />
             <span>Bookings</span>
+          </StyledNavLink>
+        </li>       
+        <li>
+          <StyledNavLink to="/cabins">
+            <HiOutlineHomeModern />
+            <span>Cabins</span>
+          </StyledNavLink>
+        </li>       
+        <li>
+          <StyledNavLink to="/users">
+            <HiOutlineUsers />
+            <span>Users</span>
+          </StyledNavLink>
+        </li>       
+        <li>
+          <StyledNavLink to="/settings">
+            <HiOutlineCog6Tooth />
+            <span>Settings</span>
           </StyledNavLink>
         </li>       
       </NavList>
