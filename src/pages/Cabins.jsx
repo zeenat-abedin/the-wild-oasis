f4df12de6b0f52809802e6de956427bb5ed15e61
zeenat <zeenu.23@gmail.com>
2024-05-25 10:04:17 +0530

    add an img to the Cabins component

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -12,6 +12,7 @@ function Cabins() {
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+      <img src="https://gmdbcmlihuxelbjjczws.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg?t=2024-05-25T04%3A29%3A01.798Z" alt="img-1" />
     </Row>
   );
 }
