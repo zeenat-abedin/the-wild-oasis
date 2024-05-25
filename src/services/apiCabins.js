718058b6f98ccdeed288c71657a4b90c09e4e960
zeenat <zeenu.23@gmail.com>
2024-05-25 09:40:42 +0530

    call getCabins API and log response to console

---
+++ src/services/apiCabins.js
@@ -0,0 +1,15 @@
+import supabase from "./supabase";
+
+export async function getCabins() {
+    
+let { data: cabins, error } = await supabase
+  .from('cabins')
+        .select('*')
+    
+    if (error) {
+        console.error(error) 
+        throw new Error('Cabins could not be loaded')
+    }
+
+    return cabins;
+}
