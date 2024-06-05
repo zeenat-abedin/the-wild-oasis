4fa9d2c8582f706e31388192a295c2ee4433a9a4
zeenat <zeenu.23@gmail.com>
2024-06-05 19:12:49 +0530

    add ReactQueryDevtools

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
 import Dashboard from './pages/Dashboard'
 import Account from './pages/Account'
 import Bookings from './pages/Bookings'
@@ -9,10 +11,20 @@ import Settings from './pages/Settings'
 import NewUsers from './pages/Users'
 import GlobalStyles from './styles/GlobalStyles'
 import AppLayout from './ui/AppLayout'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000
+    }
+  }
+})
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools initialIsOpen={false} />
     <GlobalStyles/>
     <BrowserRouter>
       <Routes>
@@ -29,7 +41,7 @@ function App() {
         <Route path="*" element={<PageNotFound />} />       
       </Routes>
     </BrowserRouter>
-    </>
+    </QueryClientProvider>
   )
 }
 
