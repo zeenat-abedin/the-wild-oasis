a8c9aa68116375a79a00ed37a7264e608669b08b
zeenat <zeenu.23@gmail.com>
2024-06-05 22:24:05 +0530

    add CabinTable

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,3 +1,4 @@
+import { useQuery } from "@tanstack/react-query";
 import styled from "styled-components";
 
 const Table = styled.div`
@@ -23,3 +24,16 @@ const TableHeader = styled.header`
   color: var(--color-grey-600);
   padding: 1.6rem 2.4rem;
 `;
+
+function CabinTable() {
+  useQuery({
+    queryKey: ['cabin']
+  })
+  return (
+    <div>
+      CabinTable
+    </div>
+  )
+}
+
+export default CabinTable
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import CabinTable from "../features/cabins/CabinTable";
 import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
@@ -9,11 +10,16 @@ function Cabins() {
   }, [])
   
   return (
+    <>
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-      <img src="https://gmdbcmlihuxelbjjczws.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg?t=2024-05-25T04%3A29%3A01.798Z" alt="img-1" />
+      <p>Filter / Sort</p>     
     </Row>
+    
+    <Row>
+      <CabinTable/>    
+    </Row>
+    </>
   );
 }
 
