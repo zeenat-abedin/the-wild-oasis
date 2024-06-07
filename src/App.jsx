c68a250233306a3acdb80fa36cefbb1656214737
zeenat <zeenu.23@gmail.com>
2024-06-07 21:46:59 +0530

    add Toaster component from react-hot-toast to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
@@ -40,7 +41,19 @@ function App() {
         <Route path="login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />       
       </Routes>
-    </BrowserRouter>
+      </BrowserRouter>
+      <Toaster position='top-center' gutter={12} containerStyle={{ margin: '8px' }} toastOptions={{
+        success: {
+          duration: 3000
+        },
+        error: {
+          duration: 5000
+        },
+        style: {
+          fontSize: '16px',
+          maxWidth: '500px'
+        }
+      }} />
     </QueryClientProvider>
   )
 }
