548b3959a15de5f0279142addd1e65ac44fd72a4
zeenat <zeenu.23@gmail.com>
2024-06-08 11:21:08 +0530

    add styles to Container component in AppLayout

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -18,6 +18,9 @@ const Main = styled.main`
 const Container = styled.div`
   max-width: 120rem;
   margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2 rem;
 `
 
 function AppLayout() {
