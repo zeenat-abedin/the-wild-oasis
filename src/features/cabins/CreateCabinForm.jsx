c8fab70d4c1251bb54e00ce1da8fd2af065c8236
zeenat <zeenu.23@gmail.com>
2024-06-10 15:53:20 +0530

    commented Error component styling

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -38,10 +38,10 @@ const Label = styled.label`
   font-weight: 500;
 `;
 
-const Error = styled.span`
-  font-size: 1.4rem;
-  color: var(--color-red-700);
-`;
+// const Error = styled.span`
+//   font-size: 1.4rem;
+//   color: var(--color-red-700);
+// `;
 
 function CreateCabinForm() {
   const { register, handleSubmit } = useForm()
