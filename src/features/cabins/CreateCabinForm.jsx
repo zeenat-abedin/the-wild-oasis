462f2d269e9a8c078b8993a00b341d0ea18b607a
zeenat <zeenu.23@gmail.com>
2024-06-12 13:21:48 +0530

    add required param

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -68,27 +68,37 @@ function CreateCabinForm() {
     <Form onSubmit={handleSubmit(onSubmit)}>
       <FormRow>
         <Label htmlFor="name">Cabin name</Label>
-        <Input type="text" id="name" {...register('name')} />
+        <Input type="text" id="name" {...register('name', {
+          required: 'This field is required'
+        })} />
       </FormRow>
 
       <FormRow>
         <Label htmlFor="maxCapacity">Maximum capacity</Label>
-        <Input type="number" id="maxCapacity" {...register('maxCapacity')}/>
+        <Input type="number" id="maxCapacity" {...register('maxCapacity', {
+          required: 'This field is required'
+        })}/>
       </FormRow>
 
       <FormRow>
         <Label htmlFor="regularPrice">Regular price</Label>
-        <Input type="number" id="regularPrice" {...register('regularPrice')}/>
+        <Input type="number" id="regularPrice" {...register('regularPrice', {
+          required: 'This field is required'
+        })}/>
       </FormRow>
 
       <FormRow>
         <Label htmlFor="discount">Discount</Label>
-        <Input type="number" id="discount" defaultValue={0} {...register('discount')}/>
+        <Input type="number" id="discount" defaultValue={0} {...register('discount', {
+          required: 'This field is required'
+        })}/>
       </FormRow>
 
       <FormRow>
         <Label htmlFor="description">Description for website</Label>
-        <Textarea type="number" id="description" defaultValue="" {...register('description')}/>
+        <Textarea type="number" id="description" defaultValue="" {...register('description', {
+          required: 'This field is required'
+        })}/>
       </FormRow>
 
       <FormRow>
