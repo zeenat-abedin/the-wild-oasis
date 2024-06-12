fc92ee876bc806f510176e1b3e38384285715e2c
zeenat <zeenu.23@gmail.com>
2024-06-12 12:05:41 +0530

    add reset form on success
    and disable Add cabin button while creating

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -1,3 +1,4 @@
+import toast from "react-hot-toast";
 import { useForm } from "react-hook-form";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import styled from "styled-components";
@@ -8,7 +9,6 @@ import Form from "../../ui/Form";
 import Button from "../../ui/Button";
 import FileInput from "../../ui/FileInput";
 import Textarea from "../../ui/Textarea";
-import toast from "react-hot-toast";
 
 const FormRow = styled.div`
   display: grid;
@@ -48,16 +48,17 @@ const Label = styled.label`
 
 function CreateCabinForm() {
   const queryClient = useQueryClient()
+  const { register, handleSubmit, reset } = useForm()
 
-  const { mutate, isLoading } = useMutation({
+  const { mutate, isLoading: isCreating } = useMutation({
     mutationFn: createCabin,
     onSuccess: () => {
       toast.success("New cabin successfully created")
       queryClient.invalidateQueries({ queryKey: ["cabins"] })
+      reset()
     },
     onError: (err) => toast.error(err.message)
   })
-  const { register, handleSubmit } = useForm()
 
   function onSubmit(data) {
     mutate(data)
@@ -99,7 +100,7 @@ function CreateCabinForm() {
         <Button variation="secondary" type="reset">
           Cancel
         </Button>
-        <Button>Add cabin</Button>
+        <Button disabled={isCreating}>Add cabin</Button>
       </FormRow>
     </Form>
   );
