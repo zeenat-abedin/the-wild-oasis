4c44457e730b75b7b94950e316f1d649ffe64461
zeenat <zeenu.23@gmail.com>
2024-06-15 22:02:55 +0530

    simplify FormRow component

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -1,7 +1,6 @@
 import toast from "react-hot-toast";
 import { useForm } from "react-hook-form";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import styled from "styled-components";
 
 import { createCabin } from "../../services/apiCabins";
 import Input from "../../ui/Input";
@@ -9,42 +8,8 @@ import Form from "../../ui/Form";
 import Button from "../../ui/Button";
 import FileInput from "../../ui/FileInput";
 import Textarea from "../../ui/Textarea";
+import FormRow from "../../ui/FormRow";
 
-const FormRow = styled.div`
-  display: grid;
-  align-items: center;
-  grid-template-columns: 24rem 1fr 1.2fr;
-  gap: 2.4rem;
-
-  padding: 1.2rem 0;
-
-  &:first-child {
-    padding-top: 0;
-  }
-
-  &:last-child {
-    padding-bottom: 0;
-  }
-
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-
-  &:has(button) {
-    display: flex;
-    justify-content: flex-end;
-    gap: 1.2rem;
-  }
-`;
-
-// const Label = styled.label`
-//   font-weight: 500;
-// `;
-
-// const Error = styled.span`
-//   font-size: 1.4rem;
-//   color: var(--color-red-700);
-// `;
 
 function CreateCabinForm() {
   const queryClient = useQueryClient()
@@ -64,7 +29,7 @@ function CreateCabinForm() {
   })
 
   function onSubmit(data) {
-    mutate({...data, image: data.image.at(0) })
+    mutate({...data, image: data.image[0] })
   }
 
   function onError(errors) {
